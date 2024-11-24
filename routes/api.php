8063a5e2e556329bde0b90e62e51aaf812a2c75f
YuriAsthar <yuriasthar@hotmail.com>
2024-11-24 00:15:32 -0300

    feat(api): cria rota para buscar users

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Router;
 
 Route::get('/ping', fn () => 'Pong');
+
+
+Route::group(['prefix' => 'users'], function (Router $router) {
+    $router->get('/', [\App\Http\Controllers\User\UserController::class, 'index'])->name('api.users.index');
+});
