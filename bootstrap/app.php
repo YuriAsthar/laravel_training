715dd65baa3e14b18c02d5a8084976d4bb401509
YuriAsthar <yuriasthar@hotmail.com>
2024-11-23 22:45:48 -0300

    feat(app): mapeia a rota api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,8 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: base_path('routes/web.php'),
+        api: base_path('routes/api.php'),
+        commands: base_path('routes/console.php'),
         health: '/up',
     )
     ->withEvents(false)
