949f2113b5f949e3e43edf161a094a7b0c1a4df6
Yuri Asthar <yuriasthar@hotmail.com>
2024-11-24 19:25:40 -0300

    change: melhora significativamente o DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,18 +11,25 @@ use App\Models\TravelRequest;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use LaravelTraining\Enums\Models\Terminal\Type;
+use LaravelTraining\Enums\Models\Transport\Type as TransportType;
 use LaravelTraining\Enums\Models\TravelRequest\Status;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Transport::factory(10)
-            ->create()
+        Transport::factory()
+            ->createMany([
+                ['type' => TransportType::AIRCRAFT],
+                ['type' => TransportType::BUS],
+            ])
             ->each(function (Transport $transport) {
-                Terminal::factory(3)
-                    ->create()
+                Terminal::factory()
+                    ->createMany([
+                        ['type' => Type::OUTWARD_TRIP],
+                        ['type' => Type::RETURN_TRIP],
+                    ])
                     ->each(fn (Terminal $terminal) => $terminal->transports()->attach($transport));
 
                 Hotel::factory(2)
@@ -37,31 +44,36 @@ class DatabaseSeeder extends Seeder
             ->createMany([
                 [
                     'name' => 'Boyka Ssj #'.random_int(1, 10),
-                    'email' => Str::uuid()->toString().'@yuriasthar.com',
+                    'email' => 'a@yuriasthar.com',
                     'password' => Hash::make('password'),
                 ],
                 [
                     'name' => 'Boyka Ssj #'.random_int(1, 10),
-                    'email' => Str::uuid()->toString().'@yuriasthar.com',
+                    'email' => 'b@yuriasthar.com',
                     'password' => Hash::make('password'),
                 ],
             ])
             ->each(function (User $user) {
-                $terminalTransport = TerminalTransport::first();
+                TerminalTransport::get()
+                    ->each(fn (TerminalTransport $terminalTransport) => TravelRequest::factory()
+                        ->for($user)
+                        ->for($terminalTransport)
+                        ->createMany([
+                            ['status' => Status::REQUESTED],
+                            ['status' => Status::APPROVED],
+                            ['status' => Status::CANCELLED],
+                        ])
+                        ->each(function (TravelRequest $travelRequest) use ($terminalTransport) {
+                            $transport = $terminalTransport->transport;
+                            $hotel = $transport->hotels()->first();
 
-                TravelRequest::factory()
-                    ->for($user)
-                    ->for($terminalTransport)
-                    ->createMany([
-                        ['status' => Status::REQUESTED],
-                        ['status' => Status::APPROVED],
-                        ['status' => Status::CANCELLED],
-                    ])
-                    ->each(function (TravelRequest $travelRequest) use ($terminalTransport) {
-                        $hotel = $terminalTransport->transport->hotels()->first();
-
-                        $travelRequest->hotelRooms()->attach($hotel->hotelRooms()->first());
-                    });
+                            $hotel->hotelRooms()
+                                ->inRandomOrder()
+                                ->limit(1)
+                                ->get()
+                                ->each(fn (HotelRoom $hotelRoom) => $travelRequest->hotelRooms()->attach($hotelRoom));
+                        }),
+                    );
             });
     }
 }
