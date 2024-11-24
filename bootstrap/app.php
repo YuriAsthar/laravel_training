bd61093908b965609a1f3469f7fb388cb0ff155c
YuriAsthar <yuriasthar@hotmail.com>
2024-11-24 00:20:23 -0300

    feat(app): habilita o mapeamento dos providers de boostrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,15 +5,14 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders()
     ->withRouting(
-        web: base_path('routes/web.php'),
-        api: base_path('routes/api.php'),
         commands: base_path('routes/console.php'),
         health: '/up',
     )
     ->withEvents(false)
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->throttleApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
