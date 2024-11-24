f020f306c8423cb13062ee694da45e3de233974d
Yuri Asthar <yuriasthar@hotmail.com>
2024-11-24 10:18:50 -0300

    feat(DatabaseSeeder): adiciona a senha ao criar um usuário

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +13,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Boyka Ssj',
             'email' => 'boykassj@yuriasthar.com',
+            'password' => Hash::make('password'),
         ]);
     }
 }
