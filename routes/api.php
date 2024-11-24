339b3f32a2f32bc29e9e1499bdcf3b0777c5f471
Yuri Asthar <yuriasthar@hotmail.com>
2024-11-24 19:27:19 -0300

    feat(api): mapeia as novas rotas para o model TravelRequest

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\TravelRequest\TravelRequestController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Routing\Router;
@@ -19,6 +20,21 @@ Route::group([
     });
 });
 
-Route::group(['prefix' => 'users'], function (Router $router) {
-    $router->get('/', [UserController::class, 'index'])->name('api.users.index');
+Route::group(['middleware' => 'auth:api'], function (Router $router) {
+    $router->group(['prefix' => 'users'], function (Router $router) {
+        $router->get('/', [UserController::class, 'index'])->name('api.users.index');
+    });
+
+    $router->group(['prefix' => 'travel-requests'], function (Router $router) {
+        $router->get('/', [TravelRequestController::class, 'index'])->name('api.travel-requests.index');
+        $router->post('/', [TravelRequestController::class, 'store'])->name('api.travel-requests.store');
+
+        $router->group(['prefix' => '{travelRequest}'], function (Router $router) {
+            $router->get('/', [TravelRequestController::class, 'show'])->name('api.travel-requests.show');
+            $router->put('/', [TravelRequestController::class, 'update'])->name('api.travel-requests.update');
+            $router->delete('/', [TravelRequestController::class, 'delete'])->name('api.travel-requests.delete');
+        });
+    });
 });
+
+
