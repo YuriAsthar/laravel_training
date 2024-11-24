cf3224ca7ee914978f336a5bbaa3c8ccb539fe4d
Yuri Asthar <yuriasthar@hotmail.com>
2024-11-24 09:29:07 -0300

    fix(ExampleTest): corrige o teste de exemplo de request para referenciar a rota ping da API

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Routing\Router;
 
-Route::get('/ping', fn () => 'Pong');
-
+Route::get('/ping', fn () => 'Pong')->name('api.ping');
 
 Route::group(['prefix' => 'users'], function (Router $router) {
     $router->get('/', [\App\Http\Controllers\User\UserController::class, 'index'])->name('api.users.index');
