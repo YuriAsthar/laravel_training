98e9d8306ee2a0ca4e6ada3356c56f221bc5ee99
YuriAsthar <yuriasthar@hotmail.com>
2024-11-23 23:13:46 -0300

    change(AppServiceProvider): modifica a configuração padrão do Carbon para usar a lang pt_BR customizada

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Carbon\Translator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Translator::get('pt_BR')->addDirectory(base_path('lang/Carbon'));
+        Carbon::setLocale('pt_BR.php');
     }
 }
