b0bb884d73ee42c20d00f080da952adb4466ae4c
Yuri Asthar <yuriasthar@hotmail.com>
2024-11-24 19:22:56 -0300

    feat: mapeia o provider AuthServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\RouteServiceProvider;
+use App\Providers\AuthServiceProvider;
 
 return [
     AppServiceProvider::class,
+    AuthServiceProvider::class,
     RouteServiceProvider::class,
 ];
