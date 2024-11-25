fbab5e332bd4c78a95f99a4915b2f25786f3e215
Yuri Asthar <yuriasthar@hotmail.com>
2024-11-25 20:48:25 -0300

    feat: cria nova lang para as notificações

---
+++ lang/pt_BR/notifications.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'travel_request' => [
+        'subject' => 'Solicitação de viagem :status!',
+        'greeting' => 'Olá, :user_name!',
+        'approved_body' => 'Sua solicitação de viagem foi **aprovada** com sucesso!',
+        'cancelled_body' => 'Infelizmente sua solicitação de viagem foi **cancelada**.',
+    ],
+];
