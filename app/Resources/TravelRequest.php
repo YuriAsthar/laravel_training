bb88dafc9d207eb00a035713deced15a9770f8aa
Yuri Asthar <yuriasthar@hotmail.com>
2024-11-24 22:54:18 -0300

    feat: mapeia as novas colunas start_date e end_date na resource de TravelRequest

--- app/Resources/TravelRequest.php
+++ app/Resources/TravelRequest.php
@@ -14,6 +14,8 @@ class TravelRequest extends JsonResource
             'amount' => $this->amount,
             'user_id' => $this->user_id,
             'terminal_transport_id' => $this->terminal_transport_id,
+            'start_date' => $this->start_date->toIso8601String(),
+            'end_date' => $this->end_date->toIso8601String(),
             'created_at' => $this->created_at->toIso8601String(),
             'deleted_at' => $this->deleted_at?->toIso8601String(),
             'user' => User::make($this->whenLoaded('user')),
