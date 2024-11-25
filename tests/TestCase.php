51aa3f17ee824db66db7bed7e7d3cccde4202f0b
Yuri Asthar <yuriasthar@hotmail.com>
2024-11-24 22:52:20 -0300

    feat: adiciona um método para facilitar a criação da token JWT nos testes da API

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function generateJwtToken(User $user, string $password = 'password'): string
+    {
+        return auth()->attempt(['email' => $user->email, 'password' => $password]);
+    }
 }
